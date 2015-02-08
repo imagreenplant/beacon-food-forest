bcfc3e4ac76ced63ff6afbd08bb5a334d7768c45
Matthew Lapora <matt@lapora.org>
2015-02-08 11:51:38 -0800

    HTML prettified files -- no functional change

--- easypop.js
+++ easypop.js
@@ -1,79 +1,85 @@
 //calendar
-function calenbox_open(){
-	window.scrollTo(0,0);
-	document.getElementById('calendar').style.display='block';
-	document.getElementById('fadeCal').style.display='block';	
+function calenbox_open() {
+    window.scrollTo(0, 0);
+    document.getElementById('calendar').style.display = 'block';
+    document.getElementById('fadeCal').style.display = 'block';
 }
-function calenbox_close(){
-	document.getElementById('calendar').style.display='none';
-	document.getElementById('fadeCal').style.display='none';
+
+function calenbox_close() {
+    document.getElementById('calendar').style.display = 'none';
+    document.getElementById('fadeCal').style.display = 'none';
 
 }
 
 //directions
-function directbox_open(){
-	window.scrollTo(0,0);
-	document.getElementById('directions').style.display='block';
-	document.getElementById('fadeDir').style.display='block';	
+function directbox_open() {
+    window.scrollTo(0, 0);
+    document.getElementById('directions').style.display = 'block';
+    document.getElementById('fadeDir').style.display = 'block';
 }
-function directbox_close(){
-	document.getElementById('directions').style.display='none';
-	document.getElementById('fadeDir').style.display='none';
-}	
+
+function directbox_close() {
+    document.getElementById('directions').style.display = 'none';
+    document.getElementById('fadeDir').style.display = 'none';
+}
 
 //donate
-function donatebox_open(){
-	window.scrollTo(0,0);
-	document.getElementById('donate').style.display='block';
-	document.getElementById('fadeDon').style.display='block';	
+function donatebox_open() {
+    window.scrollTo(0, 0);
+    document.getElementById('donate').style.display = 'block';
+    document.getElementById('fadeDon').style.display = 'block';
 }
-function donatebox_close(){
-	document.getElementById('donate').style.display='none';
-	document.getElementById('fadeDon').style.display='none';
-}	
+
+function donatebox_close() {
+    document.getElementById('donate').style.display = 'none';
+    document.getElementById('fadeDon').style.display = 'none';
+}
 
 //mailinglist
-function mailListbox_open(){
-	window.scrollTo(0,0);
-	document.getElementById('mailList').style.display='block';
-	document.getElementById('fadeMail').style.display='block';	
+function mailListbox_open() {
+    window.scrollTo(0, 0);
+    document.getElementById('mailList').style.display = 'block';
+    document.getElementById('fadeMail').style.display = 'block';
 }
-function mailListbox_close(){
-	document.getElementById('mailList').style.display='none';
-	document.getElementById('fadeMail').style.display='none';
+
+function mailListbox_close() {
+    document.getElementById('mailList').style.display = 'none';
+    document.getElementById('fadeMail').style.display = 'none';
 }
 
 // Register
-function regisbox_open(){
-	window.scrollTo(0,0);
-	document.getElementById('register').style.display='block';
-	document.getElementById('fadeReg').style.display='block';	
+function regisbox_open() {
+    window.scrollTo(0, 0);
+    document.getElementById('register').style.display = 'block';
+    document.getElementById('fadeReg').style.display = 'block';
 }
-function regisbox_close(){
-	document.getElementById('register').style.display='none';
-	document.getElementById('fadeReg').style.display='none';
+
+function regisbox_close() {
+    document.getElementById('register').style.display = 'none';
+    document.getElementById('fadeReg').style.display = 'none';
 }
 
 // Translate
-function translbox_open(){
-	window.scrollTo(0,0);
-	document.getElementById('translate').style.display='block';
-	document.getElementById('fadeTran').style.display='block';
+function translbox_open() {
+    window.scrollTo(0, 0);
+    document.getElementById('translate').style.display = 'block';
+    document.getElementById('fadeTran').style.display = 'block';
 }
-function translbox_close(){
-	document.getElementById('translate').style.display='none';
-	document.getElementById('fadeTran').style.display='none';
+
+function translbox_close() {
+    document.getElementById('translate').style.display = 'none';
+    document.getElementById('fadeTran').style.display = 'none';
 }
 
 
 // Video
-function videobox_open(){
-	window.scrollTo(0,0);
-	document.getElementById('video').style.display='block';
-	document.getElementById('fadeVid').style.display='block';	
-}
-function videobox_close(){
-	document.getElementById('video').style.display='none';
-	document.getElementById('fadeVid').style.display='none';
+function videobox_open() {
+    window.scrollTo(0, 0);
+    document.getElementById('video').style.display = 'block';
+    document.getElementById('fadeVid').style.display = 'block';
 }
 
+function videobox_close() {
+    document.getElementById('video').style.display = 'none';
+    document.getElementById('fadeVid').style.display = 'none';
+}
--- includes/includes.js
+++ includes/includes.js
@@ -1,18 +1,14 @@
-    $(function(){
-		$("#popcontent").load("includes/popcontent.html");      
-		$("#headercontent").load("includes/header.html"); 
-			$("#header_nacontent").load("includes/header-noani.html"); 
-		$("#indexcontent").load("includes/index_content.html");
-					$("#faqcontent").load("includes/faq_content.html");
-					$("#projcontent").load("includes/project_content.html");
-					$("#startcontent").load("includes/start_content.html");
-					$("#permacontent").load("includes/permaculture_c.html");					
-		$("#imgcontent").load("includes/imageBar.html");
-		$("#sidecontent").load("includes/sidenav.html");
-		$("#footercontent").load("includes/footer.html");
-			$("#footer_nacontent").load("includes/footer-noani.html");		
+    $(function() {
+        $("#popcontent").load("includes/popcontent.html");
+        $("#headercontent").load("includes/header.html");
+        $("#header_nacontent").load("includes/header-noani.html");
+        $("#indexcontent").load("includes/index_content.html");
+        $("#faqcontent").load("includes/faq_content.html");
+        $("#projcontent").load("includes/project_content.html");
+        $("#startcontent").load("includes/start_content.html");
+        $("#permacontent").load("includes/permaculture_c.html");
+        $("#imgcontent").load("includes/imageBar.html");
+        $("#sidecontent").load("includes/sidenav.html");
+        $("#footercontent").load("includes/footer.html");
+        $("#footer_nacontent").load("includes/footer-noani.html");
     });
-	
-
-	
-	
