772aea6b975f016857f97a54f8374152b3c08056
Matthew Lapora <matt@lapora.org>
2015-07-31 12:02:18 -0700

    Add high accuracy to gps watcher

--- js/map.js
+++ js/map.js
@@ -22,17 +22,28 @@ function gpsMarkerToBeaconFoodForest(myloc) {
   });
 }
 
+
+// GPS Watcher Start
 function updateGPSMarker(position) {
   if (position) {
     var updated_location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
     myloc.setPosition(updated_location);
+    log.info("New navigator location set: ", position.coords.latitude, ",", position.coords.longitude);
   }
 }
 
-var watcher = navigator.geolocation.watchPosition(function(position) {
-  log.info("New navigator location: ", position.coords.latitude, ",", position.coords.longitude);
-  updateGPSMarker(position);
-});
+function GPSerror(err) {
+  log.error("Unable to find GPS location.");
+}
+
+var watcherOptions = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+var watcher = navigator.geolocation.watchPosition(updateGPSMarker, GPSerror, watcherOptions);
+// GPS Watcher End
+
 
 function initialize() {
   var mapOptions = {
@@ -64,13 +75,15 @@ function initialize() {
   var template = [
     '<?xml version="1.0"?>',
     '<svg width="26px" height="26px" viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">',
-        '<path d="M90,45h-1.3C86.4,27.5,72.5,13.6,55,11.3V10c0-2.8-2.2-5-5-5s-5,2.2-5,5v1.3C27.5,13.6,13.6,27.5,11.3,45H10 c-2.8,0-5,2.2-5,5s2.2,5,5,5h1.3C13.6,72.5,27.5,86.4,45,88.7V90c0,2.8,2.2,5,5,5s5-2.2,5-5v-1.3C72.5,86.4,86.4,72.5,88.7,55H90 c2.8,0,5-2.2,5-5S92.8,45,90,45z M55,80.6V80c0-2.8-2.2-5-5-5s-5,2.2-5,5v0.6C31.9,78.5,21.5,68.1,19.4,55H20c2.8,0,5-2.2,5-5 s-2.2-5-5-5h-0.6C21.5,31.9,31.9,21.5,45,19.4V20c0,2.8,2.2,5,5,5s5-2.2,5-5v-0.6C68.1,21.5,78.5,31.9,80.6,45H80c-2.8,0-5,2.2-5,5 s2.2,5,5,5h0.6C78.5,68.1,68.1,78.5,55,80.6z" fill="{{ color }}">',
-          '<animateTransform attributeName="transform" type="rotate" from="0 50 50" to="90 50 50" dur="1s" repeatCount="indefinite" values="0 50 50;90 50 50;90 50 50" keyTimes="0;0.5;1"></animateTransform>',
-        '</path>',
+    '<path d="M90,45h-1.3C86.4,27.5,72.5,13.6,55,11.3V10c0-2.8-2.2-5-5-5s-5,2.2-5,5v1.3C27.5,13.6,13.6,27.5,11.3,45H10 c-2.8,0-5,2.2-5,5s2.2,5,5,5h1.3C13.6,72.5,27.5,86.4,45,88.7V90c0,2.8,2.2,5,5,5s5-2.2,5-5v-1.3C72.5,86.4,86.4,72.5,88.7,55H90 c2.8,0,5-2.2,5-5S92.8,45,90,45z M55,80.6V80c0-2.8-2.2-5-5-5s-5,2.2-5,5v0.6C31.9,78.5,21.5,68.1,19.4,55H20c2.8,0,5-2.2,5-5 s-2.2-5-5-5h-0.6C21.5,31.9,31.9,21.5,45,19.4V20c0,2.8,2.2,5,5,5s5-2.2,5-5v-0.6C68.1,21.5,78.5,31.9,80.6,45H80c-2.8,0-5,2.2-5,5 s2.2,5,5,5h0.6C78.5,68.1,68.1,78.5,55,80.6z" fill="{{ color }}">',
+    '<animateTransform attributeName="transform" type="rotate" from="0 50 50" to="90 50 50" dur="1s" repeatCount="indefinite" values="0 50 50;90 50 50;90 50 50" keyTimes="0;0.5;1"></animateTransform>',
+    '</path>',
     '</svg>'
   ].join('\n');
   var svg = template.replace('{{ color }}', '#05c400');
-  var gpsIcon2 = { url: 'data:image/svg+xml;charset=UTF-8;base64,' + btoa(svg) };
+  var gpsIcon2 = {
+    url: 'data:image/svg+xml;charset=UTF-8;base64,' + btoa(svg)
+  };
 
   myloc = new google.maps.Marker({
     clickable: false,
