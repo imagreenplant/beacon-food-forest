9464c8fdfd47047f0e70a70caac9798c7a34ab54
Matthew Lapora <matt@lapora.org>
2015-10-31 16:29:13 -0700

    Migrated home links

--- home/views.py
+++ home/views.py
@@ -1,12 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-from django.shortcuts import render_to_response
-from django.template import RequestContext
  
 def index(request):
-    # return render_to_response('home/index.html')
-    # return render(request, 'base/index.html', {})
     return render(request, 'home/home.html', {})
-    # return HttpResponse("Hi, you got there.")
