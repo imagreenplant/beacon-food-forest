73cd786b74d19ae325054a89827b017b70362ef3
Matthew Lapora <matt@lapora.org>
2015-11-13 13:08:35 -0800

    Remove new template cacher

--- beaconfoodforest/settings.py
+++ beaconfoodforest/settings.py
@@ -60,10 +60,32 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'beaconfoodforest.urls'
 
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [],
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#             'loaders': [
+#                 ('django.template.loaders.cached.Loader', [
+#                 'django.template.loaders.filesystem.Loader',
+#                 'django.template.loaders.app_directories.Loader',
+#             ]),
+#         ],
+#         },
+#     },
+# ]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -71,12 +93,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'loaders': [
-                ('django.template.loaders.cached.Loader', [
-                'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader',
-            ]),
-        ],
         },
     },
 ]
