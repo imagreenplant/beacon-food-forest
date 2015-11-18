df0a38fd699195a6cb9f4ad82c9b94c6e6577635
Matthew Lapora <matt@lapora.org>
2015-11-17 21:26:06 -0800

    Add deploy scripts

---
+++ deploy-local.sh
@@ -0,0 +1,7 @@
+cd ~/django-projects/beacon-food-forest-main/
+echo "============ Fetching new code from git =============="
+git fetch
+git rebase
+echo "============ Collecting new static files =============="
+python manage.py collectstatic --noinput
+echo "============ Finished Deployment =============="
