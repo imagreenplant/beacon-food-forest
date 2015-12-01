733742df64c467d1482856ca0bc83287269a4831
Matthew Lapora <matt@lapora.org>
2015-12-01 10:32:32 -0800

    Fix map location

--- maps/static/maps/js/map.js
+++ maps/static/maps/js/map.js
@@ -104,7 +104,7 @@ function initialize() {
 
 
     plant_map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    gml = new GeoXml("gml", plant_map, "http://beaconfoodforest.org/maps/trees_with_desc_aug-5-2015.kml", {
+    gml = new GeoXml("gml", plant_map, "s/maps/kml/trees_with_desc_aug-5-2015.kml", {
         sidebarid: "the_side_bar",
         quiet: true, //Removes on-screen dialogs, puts in console.log
         // pointlabelclass: "point-label",  In documentation, but not functioning
