15c3c1f593cbcf416b9fcd81cf0738804136c411
Matthew Lapora <matt@lapora.org>
2015-12-02 19:48:08 -0800

    Change local static url and fix settings_local.py

--- beaconfoodforest/settings_local.py
+++ beaconfoodforest/settings_local.py
@@ -1,10 +1,14 @@
 #Settings for Local environment
+import os
 
 DEBUG = True
 print "Overriding production config to local, Setting DEBUG to True"
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ALLOWED_HOSTS = [
     '*',  # Allow all domains
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static-collector')
+STATIC_URL = '/local-static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'local-static')
