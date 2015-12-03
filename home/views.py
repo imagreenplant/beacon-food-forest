b09b49abd582024fd666ec60d6e13bad3220e802
Matthew Lapora <matt@lapora.org>
2015-12-02 19:54:21 -0800

    Change homepage to render to response

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
  
 def index(request):
-    return render(request, 'home/home.html', {})
+    return render_to_response('home/home.html')
