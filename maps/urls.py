422c1508098f6ead6dbfe48c8041bcd30b59fb23
Matthew Lapora <matt@lapora.org>
2015-12-03 10:28:23 -0800

    Fix url for tree map so it doesn't catch every sub url

--- maps/urls.py
+++ maps/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^trees/', views.trees , name="trees"),
+    url(r'^trees/$', views.trees , name="trees"),
 ]
