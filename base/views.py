4b45c756ebf0545da9b6782aabbafcc7b96e3598
Matthew Lapora <matt@lapora.org>
2015-12-13 12:04:25 -0800

    Add a few more debug niceties

--- base/views.py
+++ base/views.py
@@ -6,5 +6,13 @@ def robots(request):
 	return render_to_response('base/robots.txt')
 
 def debuginfo(request):
-	content = "Environment is set to %s" % settings.ENVIRONMENT
+	content = [
+		"Environment is set to %s" % settings.ENVIRONMENT,
+		"Debug is set to %s" % settings.DEBUG,
+		"Static root is %s" % settings.STATIC_ROOT,
+		"Static url is %s" % settings.STATIC_URL,
+		"Cache type is %s" % settings.CACHES['default']['BACKEND'],
+	]
+
+	content = "\n".join(content)
 	return HttpResponse(content, content_type='text/plain')
