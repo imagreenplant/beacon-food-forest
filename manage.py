b70a9467e08fc7902e3f6be3b736ba2a4ff63eb9
Matthew Lapora <matt@lapora.org>
2015-12-21 20:58:14 -0800

    Change the way static files are served locally for easier development

--- manage.py
+++ manage.py
@@ -2,6 +2,11 @@
 import os
 import sys
 
+# Adds static file hack from this:  
+# http://stackoverflow.com/questions/7013735/turn-off-caching-of-static-files-in-django-development-server
+if '--nostatic' not in sys.argv:
+    sys.argv.append('--nostatic')
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "beaconfoodforest.settings")
 
