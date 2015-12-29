400af221373951e8727094ee6a49f49aad6f5495
Matthew Lapora <matt@lapora.org>
2015-12-28 21:28:08 -0800

    Modify testing scripts to deploy current local branch

--- scripts/TESTING-deploy-on-remote-bluehost-server.sh
+++ scripts/TESTING-deploy-on-remote-bluehost-server.sh
@@ -1,1 +1,8 @@
-ssh beaconf2@beaconfoodforest.org '~/django-projects/test/beacon-food-forest-main/scripts/TESTING-deploy-on-local-server.sh'
+echo "===================================================="
+echo "     Make sure you are on the branch you want"
+echo "   to deploy to test.beaconfoodforest.org when"
+echo "             running this script."
+echo "===================================================="
+CURRENT_BRANCH=`git symbolic-ref --short -q HEAD`
+git push origin CURRENT_BRANCH
+ssh beaconf2@beaconfoodforest.org "~/django-projects/test/beacon-food-forest-main/scripts/TESTING-deploy-on-local-server.sh $CURRENT_BRANCH"
