f65ddafdfb45fabeab39456409619ec8972d5186
Matthew Lapora <matt@lapora.org>
2015-12-30 11:56:39 -0800

    Add condition if no articles, and add limit of 5 articles

--- base/templatetags/news_articles.py
+++ base/templatetags/news_articles.py
@@ -7,6 +7,6 @@ register = template.Library()
 @register.inclusion_tag('base/external_news_articles.html')
 def show_recent_articles():
 	recent_external_articles = ExternalNewsArticle.objects.filter(pub_date__gt=datetime.datetime.now() - \
-		datetime.timedelta(weeks=52)).order_by('-pub_date')
+		datetime.timedelta(weeks=52)).order_by('-pub_date')[:5]
 	
 	return {'recent_articles':recent_external_articles}
