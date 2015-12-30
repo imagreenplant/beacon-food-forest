4f5ac537e4f196ff8f0e2cede29fef6aca522534
Matthew Lapora <matt@lapora.org>
2015-12-30 07:33:53 -0800

    Cleanup admin page for articles

--- base/admin.py
+++ base/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import ExternalNewsArticle
 
-admin.site.register(ExternalNewsArticle)
+@admin.register(ExternalNewsArticle)
+class ExternalNewsArticleAdmin(admin.ModelAdmin):
+	list_display = ('external_publisher', 'news_slug', 'pub_date')
+
+
--- base/models.py
+++ base/models.py
@@ -11,7 +11,7 @@ class ExternalNewsArticle(models.Model):
     def __str__(self):
         return ": ".join([self.external_publisher,self.news_slug, self.pub_date.strftime('%b %d,%Y')])
 
-    external_publisher = models.CharField(max_length=100, blank=False)
-    news_slug = models.CharField(max_length=500, blank=True)
-    external_url = models.URLField(blank=False)
-    pub_date = models.DateField(auto_now=False, auto_now_add=False, blank=False, default=timezone.now)
+    external_publisher = models.CharField(max_length=100, blank=False, help_text="The publisher of the article")
+    news_slug = models.CharField(max_length=500, blank=True, help_text="A short description of article")
+    external_url = models.URLField(blank=False, help_text="The URL at which the article resides")
+    pub_date = models.DateField(auto_now=False, auto_now_add=False, blank=False, default=timezone.now, help_text="The date the article was originally published")
