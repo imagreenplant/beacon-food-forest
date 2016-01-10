5a9a54c4b4aacf21f6a4bc9914018b1bf2a2f679
Matthew Lapora <matt@lapora.org>
2016-01-09 23:58:22 -0800

    Add other choice

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,6 @@
 from django import forms  
 
-DONATION_CHOICES = (("food","Food"),("plants","Plants and Materials"))
+DONATION_CHOICES = (("food","Food"),("plants","Plants and Materials"),("other","Other"))
 
 class MaterialsDonationForm(forms.Form):
     your_name = forms.CharField(label='Your First and Last Name', max_length=100, required=True ,
