7d7f44bf484116a10bc71688cf1b9f9e40d41e60
Matthew Lapora <matt@lapora.org>
2016-01-22 23:11:54 -0800

    Final styling of form with captcha

--- base/forms.py
+++ base/forms.py
@@ -5,11 +5,12 @@ DONATION_CHOICES = (("food","Food"),("plants","Plants and Materials"),("other","
 
 class MaterialsDonationForm(forms.Form):
     your_name = forms.CharField(label='Your First and Last Name', max_length=100, required=True ,
-    	widget=forms.TextInput(attrs={'autofocus': '', 'required': 'required', 'placeholder': 'Your name'}))
-    your_email = forms.EmailField(label='Email', required=True)
+    	widget=forms.TextInput(attrs={'autofocus': '', 'required': 'required', 'placeholder': 'Your name', 'class':'full-width'}))
+    your_email = forms.EmailField(label='Email', required=True, widget=forms.EmailInput(
+    	attrs={'required': 'required', 'placeholder': 'Your email', 'class':'full-width'}))
     donation_type = forms.ChoiceField(label="Please Select a Donation Type", required=True, choices=DONATION_CHOICES )
-    donation_captcha = CaptchaField(label='Please type in the letters you see on the right:')
-    donation_description = forms.CharField(label='Donation Type', max_length=2000, required=True, 
-    	widget=forms.Textarea(attrs={'required': 'required', 'placeholder': 'A short description of your donation:',
+    donation_description = forms.CharField(label='Please describe the donation:', max_length=2000, required=True, 
+    	widget=forms.Textarea(attrs={'required': 'required', 'placeholder': 'Description here:',
     		'rows':'4'}))
+    donation_captcha = CaptchaField(label='To prove you are human, please solve this math problem:',required=True)
     
