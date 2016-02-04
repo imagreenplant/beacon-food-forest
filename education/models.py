7f6dc8dc3a4c52f758a052e709f7676d341feedd
Matthew Lapora <matt@lapora.org>
2016-02-04 12:27:40 -0500

    Added some help to Education screen

--- education/models.py
+++ education/models.py
@@ -5,8 +5,8 @@ import datetime
 
 class ClassEvent(models.Model):
 	title = models.CharField('class title', max_length=200, blank=False)
-	publish_date = models.DateTimeField('date to publicize', default=timezone.now)
-	expire_date = models.DateTimeField('date to publicize', default=timezone.now)
+	publish_date = models.DateTimeField('date to publicize posting', default=timezone.now)
+	expire_date = models.DateTimeField('date to expire posting', default=timezone.now)
 	event_date = models.DateField('date class is held', blank=False)
 	event_start_time = models.TimeField('time class starts', blank=True)
 	event_end_time = models.TimeField('time class ends', blank=True)
@@ -15,7 +15,8 @@ class ClassEvent(models.Model):
 	class_primary_image = models.ImageField("optional class image", blank=True)
 	class_location = models.CharField("text of location", max_length=300, default="S. Dakota Street and 16th Avenue South")
 	class_location_link = models.URLField("map service link to class", default="https://goo.gl/maps/fpdzyHy5kjr")
-	class_slug_url = models.CharField(max_length=500, blank=True, help_text="An url friendly short description")
+	################ needs to be unique
+	class_slug_url = models.CharField(max_length=500, blank=False, help_text="An url friendly short description.  Must be unique to each class.")
 
 	# Note: must use Python3 for __str__ to work
 	def __str__(self):
