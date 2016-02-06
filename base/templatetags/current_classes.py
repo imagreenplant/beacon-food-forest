fb190cebd982cd0ecfad9827edadb827dce3d0b6
Matthew Lapora <matt@lapora.org>
2016-02-06 03:04:20 -0800

    Fixed date publish filter

--- base/templatetags/current_classes.py
+++ base/templatetags/current_classes.py
@@ -5,8 +5,8 @@ register = template.Library()
 
 @register.inclusion_tag('base/current_classes.html')
 def show_current_classes():
-	current_classes = ClassEvent.objects.filter(publish_date__lt=datetime.date.today()) \
-		.filter(expire_date__gt=datetime.date.today()) \
+	current_classes = ClassEvent.objects.filter(publish_date__lte=datetime.date.today()) \
+		.filter(expire_date__gte=datetime.date.today()) \
 		.order_by('publish_date')[:10]
 
 	return {'current_classes':current_classes}
