2f9489d81ae6ade69c276891341b5e5424d0d8bc
Matthew Lapora <matt@lapora.org>
2016-02-09 21:48:00 -0800

    Add expiry padding to classes and work parties to show it for a few more days

--- base/templatetags/current_classes.py
+++ base/templatetags/current_classes.py
@@ -5,8 +5,11 @@ register = template.Library()
 
 @register.inclusion_tag('base/current_classes.html')
 def show_current_classes():
+	days_shown_past_expiry = 3
+	practical_expire_date = datetime.datetime.now() - datetime.timedelta(days=days_shown_past_expiry)
+
 	current_classes = ClassEvent.objects.filter(publish_date__lte=datetime.date.today()) \
-		.filter(expire_date__gte=datetime.date.today()) \
+		.filter(expire_date__gte=practical_expire_date) \
 		.order_by('publish_date')[:10]
 
 	return {'current_classes':current_classes}
