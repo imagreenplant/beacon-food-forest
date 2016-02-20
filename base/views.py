1ae38777a506d268440b23ae88d1eb61eef8194f
Matthew Lapora <matt@lapora.org>
2016-02-19 21:43:58 -0800

    Python version output to debug

--- base/views.py
+++ base/views.py
@@ -7,12 +7,15 @@ from beaconfoodforest import settings
 from base.forms import MaterialsDonationForm
 from base.models import Download
 
+import sys
+
 def robots(request):
 	return render_to_response('base/robots.txt')
 
 def debuginfo(request):
 	content = [
 		"Environment is set to %s" % settings.ENVIRONMENT,
+		"Python version is %s" % sys.version,
 		"Debug is set to %s" % settings.DEBUG,
 		"Static root is %s" % settings.STATIC_ROOT,
 		"Static url is %s" % settings.STATIC_URL,
