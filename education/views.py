2f887d9ffb5beb40cf86f43188b3556311b316be
Matthew Lapora <matt@lapora.org>
2016-03-21 22:06:54 -0700

    Fix excess migrations for education

--- education/views.py
+++ education/views.py
@@ -30,25 +30,6 @@ def current_classes(request):
 		{'classes':current_classes, 'title':page_title},\
 		context_instance = RequestContext(request))
 
-def past_classes(request):
-	page_title = "Past Classes"
-	class_events = ClassEvent.objects.filter(expire_date__lt=datetime.date.today()) \
-										.order_by('publish_date')
-	return render_to_response('education/class_list.html',\
-		{'classes':class_events,'title':page_title}, \
-		context_instance = RequestContext(request))
-
-
-def current_classes(request):
-	page_title = "Upcoming Classes"
-	current_classes = ClassEvent.objects.filter(publish_date__lte=datetime.date.today()) \
-										.filter(expire_date__gte=datetime.date.today()) \
-										.order_by('publish_date')
-
-	return render_to_response('education/class_list.html',\
-		{'classes':current_classes, 'title':page_title},\
-		context_instance = RequestContext(request))
-
 def teacher_criteria(request):
 	return render_to_response('education/teacher_criteria.html',{},context_instance = RequestContext(request))
 
