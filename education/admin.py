7324aac3c97f429059289b6f1b3e30665aad3a53
Matthew Lapora <matt@lapora.org>
2016-04-04 22:21:16 -0700

    add prepopulated field

--- education/admin.py
+++ education/admin.py
@@ -10,3 +10,4 @@ from education.models import ClassEvent
 admin.site.register(ClassEvent)
 class ClassEventAdmin(MarkdownModelAdmin):
 	list_display = ('title', 'class_slug_url', 'publish_date')
+	prepopulated_fields = {"class_slug_url": ("title",)}
