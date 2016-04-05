f2faa6dabbc29e456978c76c64a230b0b6ccd931
Matthew Lapora <matt@lapora.org>
2016-04-04 21:14:06 -0700

    separated coordinates from friendly locations and updated slug

--- plants/admin.py
+++ plants/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_markdown.admin import MarkdownModelAdmin
-from plants.models import Plant, Location
+from plants.models import Plant, Location, Coordinates
 # from plants.models import MaintenanceEvent, Harvest
 
 @admin.register(Plant)
@@ -10,6 +10,13 @@ class PlantAdmin(MarkdownModelAdmin):
 @admin.register(Location)
 class LocationAdmin(MarkdownModelAdmin):
 	list_display = ('friendly_location', 'gps_latitude', 'gps_longitude')
+
+@admin.register(Coordinates)
+class CoordinatesAdmin(admin.ModelAdmin):
+    '''
+        Admin View for Coordinates
+    '''
+    list_display = ('latitude','longitude')
 
 # @admin.register(MaintenanceEvent)
 # class MaintenanceEventAdmin(admin.ModelAdmin):
