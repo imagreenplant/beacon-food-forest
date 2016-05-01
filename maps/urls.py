5f39023b3e78d12e300a500d2bf764925e15cc31
Matthew Lapora <matt@lapora.org>
2016-05-01 14:49:47 -0700

    add views and urls for kml maps

--- maps/urls.py
+++ maps/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^trees/$', views.trees , name="trees"),
     url(r'^phase2/$', views.phase2 , name="phase2"),
+    url(r'^(?P<slug>[\w-]+)/$', 'views.kml_map', name="kml_map"),
 ]
--- maps/views.py
+++ maps/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.template import RequestContext
+from .models import KmlMap
 
 def trees(request):
 	return render(request, 'maps/map.html', {}, context_instance=RequestContext(request))
 
 def phase2(request):
 	return render(request, 'maps/phase2.html', {}, context_instance=RequestContext(request))
+
+def kml_map(request,slug):
+	kmap = get_object_or_404(KmlMap, slug=slug)
+    return render_to_response('maps/kmap.html', {'kml':kml},context_instance = RequestContext(request))
+	
