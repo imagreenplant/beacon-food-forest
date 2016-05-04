1d88c627f78cbf98c48ca02cc99ce2905034b65c
Matthew Lapora <matt@lapora.org>
2016-05-04 14:45:57 -0700

    Remove commented urls

--- maps/urls.py
+++ maps/urls.py
@@ -2,8 +2,5 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    # url(r'^trees/$', views.trees , name="trees"),
-    # url(r'^phase2/$', views.phase2 , name="phase2"),
-    # url(r'^test/$', views.test , name="test-map"),
     url(r'^(?P<slug>[\w-]+)/$', views.kml_map, name="kml_map"),
 ]
