177c13f9b0968bbd6a6e3fd63946ce0b3930814d
Matthew Lapora <matt@lapora.org>
2016-05-27 11:23:42 -0700

    Add upcoming classes to announcements

--- base/templatetags/announcements.py
+++ base/templatetags/announcements.py
@@ -5,18 +5,25 @@ from base.models import ExternalNewsArticle, WorkPartyEvent, Announcement
 register = template.Library()
 
 def getUpcomingWorkParties():
-	display_delta = datetime.datetime.now() + datetime.timedelta(days=5)
+	days_before_workparty_to_show_announce = 5
+	display_delta = datetime.datetime.now() + datetime.timedelta(days=days_before_workparty_to_show_announce)
 	work_parties = WorkPartyEvent.objects.filter(work_party_date__lte=display_delta)\
 		.filter(work_party_date__gte=datetime.date.today())
 	return work_parties
 
 def getNewClasses():
-	# Returns any classes newly published within n recency_days
-	return None
+	days_to_show_new_class = 3
+	display_delta = datetime.datetime.now() - datetime.timedelta(days=days_to_show_new_class)
+	new_classes = ClassEvent.objects.filter(publish_date__gte=display_delta)\
+		.filter(publish_date__lte=datetime.date.today())
+	return new_classes
 	
 def getUpcomingClasses():
-	# Returns any classes coming up in n recency_days
-	return None
+	days_before_class_to_show = 4
+	display_delta = datetime.datetime.now() + datetime.timedelta(days=days_before_class_to_show)
+	upcoming_classes = ClassEvent.objects.filter(event_date__lte=display_delta)\
+		.filter(event_date__gte=datetime.date.today())
+	return upcoming_classes
 
 def getRecentArticles():
 	# Returns any articles recently published within n recency_days
