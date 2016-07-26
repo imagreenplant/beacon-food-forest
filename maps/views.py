f189737e7dc40155909e7c50b3fad88f4480c483
Matthew Lapora <matt@lapora.org>
2016-07-26 15:07:41 -0700

    Add category map template and hooked up in view

--- maps/views.py
+++ maps/views.py
@@ -4,16 +4,24 @@ from .models import KmlMap
 from plants.models import Plant
 
 def kml_map(request,slug):
+	# The kml map is a useful tool to allow users to upload their KML from Google Earth and have it show online.
 	kml = get_object_or_404(KmlMap, slug=slug)
 	return render_to_response('maps/kmap.html', {'kml':kml}, context_instance=RequestContext(request))
 
 def tag_map(request,tag):
-	kml = get_object_or_404(TagMap, tag=tag)
-	return render_to_response('maps/tagmap.html', {'tag':tag}, context_instance=RequestContext(request))
+	# The tag map is a map based on a tag cloud.  I added this but not sure how to proceed design wise yet.
+	if tag in Plant.tags.slugs():
+		# Makes assumption can filter on slug.  May need to convert filter query
+		tagged_plants = Plant.objects.filter(tags__name__in=[tag])
+		# Find a way to convert to SEO readable title
+		site_title_append = tag
+	return render_to_response('maps/tagmap.html', {'site_title_append':site_title_append}, \
+		context_instance=RequestContext(request))
 
 def fruitTrees(request):
 	# View specific to fruit trees only, as the main featured map
-	fruit_trees = Plant.objects.filter(tags__name__in=["edible fruit"])
-	tag = "Fruit Trees"
-	return render_to_response('maps/tagmap.html', {'plants':fruit_trees, 'tag': tag}, context_instance=RequestContext(request))
+	fruit_trees = Plant.objects.filter(category__category__exact = "Trees")
+	site_title_append = "Trees"
+	return render_to_response('maps/categorymap.html', {'plants':fruit_trees, \
+		'site_title_append': site_title_append }, context_instance=RequestContext(request))
 	
