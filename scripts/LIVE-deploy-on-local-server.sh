c2fc0c56c4efd03d0ccbc75a104813ed4551f214
Matthew Lapora <matt@lapora.org>
2016-09-20 12:30:20 -0700

    Update live script with edits from test environment

--- scripts/LIVE-deploy-on-local-server.sh
+++ scripts/LIVE-deploy-on-local-server.sh
@@ -1,7 +1,20 @@
 cd ~/django-projects/beacon-food-forest-main/
+echo "============ Stashing local changes =============="
+git stash
 echo "============ Fetching new code from git =============="
 git fetch
 git rebase
+echo "============ Applying virtual environment =============="
+export WORKON_HOME=$HOME/.python-environments
+export PROJECT_HOME=$HOME/django-projects
+export VIRTUALENVWRAPPER_PYTHON='/home3/beaconf2/python3.5/bin/python3.5'
+source /home3/beaconf2/bin/virtualenvwrapper.sh
+workon bff-py3.5
+cd ~/django-projects/beacon-food-forest-main/
+echo "============ UN-Stashing local changes =============="
+git stash pop
+echo "============ Installing new requirements =============="
+pip install -r requirements.txt
 echo "============ Collecting new static files =============="
 python manage.py collectstatic --noinput
 echo "============ Migrating database schema =============="
