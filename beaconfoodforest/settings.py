1c4da66c98db8e4ae825db1f158247d206ea7879
Matthew Lapora <matt@lapora.org>
2016-09-29 22:38:12 -0700

    PEP8 changes to settings

--- beaconfoodforest/settings.py
+++ beaconfoodforest/settings.py
@@ -157,7 +157,8 @@ LOGGING = {
     },
     'formatters': {
         'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+            'format': '%(levelname)s %(asctime)s %(module)s \
+                       %(process)d %(thread)d %(message)s'
         },
     },
     'loggers': {
