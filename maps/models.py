d00efb23ed85e7c5615c8e818c9db3f662a8fc70
Matthew Lapora <matt@lapora.org>
2016-10-01 23:55:26 -0700

    PEP8 cleanup for map models

--- maps/models.py
+++ maps/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 
+
 class KmlMap(models.Model):
     def __str__(self):
         return self.name
@@ -8,11 +9,13 @@ class KmlMap(models.Model):
     def get_absolute_url(self):
         return reverse('kml-map', args=[str(self.slug)])
 
-    kml = models.FileField(upload_to="kml/",max_length=200, help_text='Upload file here')
-    active = models.BooleanField(blank=False, default=True, help_text='Should this entry be live on the site?')
-    name = models.CharField(max_length=100, blank=False, default="", help_text="A short descriptive display name")
-    description = models.CharField(max_length=500, blank=True, help_text="Optional description for map")
-    slug = models.SlugField(unique=True, blank=False, help_text="Unique url-friendly text to locate map e.g maps/<here>/ where here is 'fruits' etc")
-
-    
-
+    kml = models.FileField(upload_to="kml/", max_length=200, help_text='Upload file here')
+    active = models.BooleanField(blank=False, default=True,
+                                 help_text='Should this entry be live on the site?')
+    name = models.CharField(max_length=100, blank=False, default="",
+                            help_text="A short descriptive display name")
+    description = models.CharField(max_length=500, blank=True,
+                                   help_text="Optional description for map")
+    slug = models.SlugField(unique=True, blank=False,
+                            help_text="Unique url-friendly text to locate map e.g \
+                            maps/<here>/ where here is 'fruits' etc")
