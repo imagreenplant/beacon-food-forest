7c3946820aa8aace6ae26a33bed1f24bd1966f4b
Matthew Lapora <matt@lapora.org>
2016-10-01 23:41:51 -0700

    PEP8 cleanup

--- maps/urls.py
+++ maps/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^fruit-trees/$', views.fruitTrees, name="fruit-trees"),
-	url(r'^(?P<tag>[\w-]+)/$', views.tag_map, name="tag-map"),
+    url(r'^fruit-trees/$', views.fruitTrees, name="fruit-trees"),
+    url(r'^(?P<tag>[\w-]+)/$', views.tag_map, name="tag-map"),
     url(r'^kml/(?P<slug>[\w-]+)/$', views.kml_map, name="kml-map"),
 ]
