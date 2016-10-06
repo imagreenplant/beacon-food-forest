b4b18c9243f69468f3cfcf18c6d67c32cb69a233
Matthew Lapora <matt@lapora.org>
2016-10-06 15:43:10 -0700

    Fixed problems when using strict mode

--- maps/static/maps/js/mapdb.js
+++ maps/static/maps/js/mapdb.js
@@ -1,31 +1,35 @@
+"use strict";
+
+var google = this.google;
+
 var all_markers = [];
 
-var contentString = '<div id="content">'+
-    '<div id="siteNotice">'+
-    '</div>'+
-    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-    '<div id="bodyContent">'+
+var contentString = '<div id="content">' +
+    '<div id="siteNotice">' +
+    '</div>' +
+    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+    '<div id="bodyContent">' +
     '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-    'sandstone rock formation in the southern part of the '+
-    'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-    'south west of the nearest large town, Alice Springs; 450&#160;km '+
-    '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-    'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-    'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-    'Aboriginal people of the area. It has many springs, waterholes, '+
-    'rock caves and ancient paintings. Uluru is listed as a World '+
-    'Heritage Site.</p>'+
-    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-    'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-    '(last visited June 22, 2009).</p>'+
-    '</div>'+
+    'sandstone rock formation in the southern part of the ' +
+    'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
+    'south west of the nearest large town, Alice Springs; 450&#160;km ' +
+    '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
+    'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
+    'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
+    'Aboriginal people of the area. It has many springs, waterholes, ' +
+    'rock caves and ancient paintings. Uluru is listed as a World ' +
+    'Heritage Site.</p>' +
+    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+    'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
+    '(last visited June 22, 2009).</p>' +
+    '</div>' +
     '</div>';
 
 var infowindow = new google.maps.InfoWindow({
-	content: contentString
+    content: contentString
 });
 
-function addMarker(location, data) {
+function addMarker(location, data, map) {
     var marker = new google.maps.Marker({
         position: location,
         map: map,
@@ -35,19 +39,15 @@ function addMarker(location, data) {
     all_markers.push(marker);
 }
 
-function addAllMarkers(markers) {
+function addAllMarkers(markers,map) {
     for (var marker in markers) {
-        addMarker(markers[marker].coordinates, marker)
+        addMarker(markers[marker].coordinates, marker,map)
     }
 }
 
 function clickMarker(argument) {
-	// body...
+    // body...
 }
-
-
-
-
 
 function initMap() {
     var beacon_food_forest_location = new google.maps.LatLng(47.56845610052802, -122.31254031038299);
@@ -72,8 +72,8 @@ function initMap() {
         overviewMapControl: false,
     };
 
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-    addAllMarkers(plants)
+    addAllMarkers(plants,map);
 
 }
