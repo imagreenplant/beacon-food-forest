47087e68df4119d65f20fd6c00e96626c7debf0d
Matthew Lapora <matt@lapora.org>
2016-10-06 23:30:19 -0700

    Working on converting to js namespace

--- maps/static/maps/js/mapdb.js
+++ maps/static/maps/js/mapdb.js
@@ -1,8 +1,62 @@
 "use strict";
-
+var entityMap = entityMap || {};
 var google = this.google;
+var plants = this.plants;
 
-var all_markers = [];
+var beacon_food_forest_location = new google.maps.LatLng(47.56845610052802, -122.31254031038299);
+
+var entityMap = {
+    getInfo: function() { /**/ },
+
+    entities: [],
+    map: {},
+
+
+    mapOptions: {
+        zoom: 18,
+
+        //Heading can't be used here, appears to conflict with GeoXML lib.  Stuck with North/South orientation.
+        center: beacon_food_forest_location,
+        mapTypeId: google.maps.MapTypeId.SATELLITE,
+        tilt: 0,
+        panControl: false,
+        zoomControl: true,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.SMALL,
+            position: google.maps.ControlPosition.LEFT_BOTTOM,
+        },
+
+        // May be useful someday if we use an overlay as a map, instead of the satellite
+        mapTypeControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        overviewMapControl: false,
+    },
+
+    addAllMarkers: function(markers) {
+        for (var marker in markers) {
+            this.addMarker(markers[marker].coordinates, marker);
+        }
+    },
+
+    addMarker: function(location, data) {
+        var marker = new google.maps.Marker({
+            position: location,
+            map: this.map,
+            title: data.name,
+            // infowindow: data.content,
+        });
+        this.entities.push(marker);
+    },
+
+    initMap: function() {
+        this.map = new google.maps.Map(document.getElementById('map-canvas'), this.mapOptions);
+        this.addAllMarkers(plants);
+    },
+
+};
+
+// var all_markers = [];
 
 var contentString = '<div id="content">' +
     '<div id="siteNotice">' +
@@ -25,55 +79,83 @@ var contentString = '<div id="content">' +
     '</div>' +
     '</div>';
 
-var infowindow = new google.maps.InfoWindow({
+var infowindow = google.maps.InfoWindow({
     content: contentString
 });
 
-function addMarker(location, data, map) {
-    var marker = new google.maps.Marker({
-        position: location,
-        map: map,
-        title: data.name,
-        infowindow: data.content,
-    });
-    all_markers.push(marker);
-}
+var plantMap = new entityMap();
 
-function addAllMarkers(markers,map) {
-    for (var marker in markers) {
-        addMarker(markers[marker].coordinates, marker,map)
-    }
-}
+// function addMarker(location, data, map) {
+//     var marker = new google.maps.Marker({
+//         position: location,
+//         map: map,
+//         title: data.name,
+//         infowindow: data.content,
+//     });
+//     all_markers.push(marker);
+// }
 
-function clickMarker(argument) {
-    // body...
-}
+// function addAllMarkers(markers,map) {
+//     for (var marker in markers) {
+//         addMarker(markers[marker].coordinates, marker,map)
+//     }
+// }
 
-function initMap() {
-    var beacon_food_forest_location = new google.maps.LatLng(47.56845610052802, -122.31254031038299);
-    var mapOptions = {
-        zoom: 18,
+// function clickMarker(argument) {
+//     // body...
+// }
 
-        //Heading can't be used here, appears to conflict with GeoXML lib.  Stuck with North/South orientation.
-        center: beacon_food_forest_location,
-        mapTypeId: google.maps.MapTypeId.SATELLITE,
-        tilt: 0,
-        panControl: false,
-        zoomControl: true,
-        zoomControlOptions: {
-            style: google.maps.ZoomControlStyle.SMALL,
-            position: google.maps.ControlPosition.LEFT_BOTTOM,
-        },
+// function initMap() {
+//     var beacon_food_forest_location = new google.maps.LatLng(47.56845610052802, -122.31254031038299);
+//     var mapOptions = {
+//         zoom: 18,
 
-        // May be useful someday if we use an overlay as a map, instead of the satellite
-        mapTypeControl: false,
-        scaleControl: false,
-        streetViewControl: false,
-        overviewMapControl: false,
-    };
+//         //Heading can't be used here, appears to conflict with GeoXML lib.  Stuck with North/South orientation.
+//         center: beacon_food_forest_location,
+//         mapTypeId: google.maps.MapTypeId.SATELLITE,
+//         tilt: 0,
+//         panControl: false,
+//         zoomControl: true,
+//         zoomControlOptions: {
+//             style: google.maps.ZoomControlStyle.SMALL,
+//             position: google.maps.ControlPosition.LEFT_BOTTOM,
+//         },
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+//         // May be useful someday if we use an overlay as a map, instead of the satellite
+//         mapTypeControl: false,
+//         scaleControl: false,
+//         streetViewControl: false,
+//         overviewMapControl: false,
+//     };
 
-    addAllMarkers(plants,map);
+//     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-}
+//     addAllMarkers(plants,map);
+
+// }
+
+
+
+// var myApplication = {
+//     getInfo:function(){ /**/ },
+
+//     // we can also populate our object literal to support
+//     // further object literal namespaces containing anything
+//     // really:
+//     models : {},
+//     views : {
+//         pages : {}
+//     },
+//     collections : {}
+// };
+// myApplication.foo = function(){
+//     return "bar";
+// }
+// myApplication.utils = {
+//     toString:function(){
+//         /*..*/
+//     },
+//     export: function(){
+//         /*..*/
+//     }
+// }
