3198053e3518d75c0641d837dba70759765c7830
Matthew Lapora <matt@lapora.org>
2016-10-18 09:34:42 -0700

    Add 404 and 500 basic pages

--- base/views.py
+++ base/views.py
@@ -84,3 +84,17 @@ def announcement(request, slug):
     announcement = get_object_or_404(Announcement, slug=slug)
     return render_to_response('base/announcement_detail.html', {'announcement': announcement},
                               context_instance=RequestContext(request))
+
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
