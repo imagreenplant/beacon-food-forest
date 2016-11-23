0be5ed3ab98ce1f3a0b07a43fcaa716c9dfdb8fe
Matthew Lapora <github@thelaporas.com>
2016-11-23 13:48:12 -0800

    PEP8 fixes

--- plants/views.py
+++ plants/views.py
@@ -1,31 +1,33 @@
-from django.shortcuts import render
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from .models import Plant
 from django.core.exceptions import ObjectDoesNotExist
 
+
 def getCanonicalPlantUrl(plant_obj):
-	if plant_obj.url_slug:
-		return plant_obj.url_slug
-	else:
-		return plant_obj.site_code
+    if plant_obj.url_slug:
+        return plant_obj.url_slug
+    else:
+        return plant_obj.site_code
+
 
 def index(request):
-	plants = Plant.objects.all()
-	page_title = "All Plants"
+    plants = Plant.objects.all()
+    page_title = "All Plants"
 
-	for plant in plants:
-		plant.canonical = getCanonicalPlantUrl(plant)
+    for plant in plants:
+        plant.canonical = getCanonicalPlantUrl(plant)
 
-	return render_to_response('plants/all_plants.html',{'title':page_title,'plants':plants}, \
-		context_instance = RequestContext(request))
+    return render_to_response('plants/all_plants.html', {'title': page_title, 'plants': plants},
+                              context_instance=RequestContext(request))
 
-def detail(request,id):
-	try:
-		plant = Plant.objects.get(url_slug=id)
-	except ObjectDoesNotExist:
-		plant = get_object_or_404(Plant, site_code=id)
 
-	return render_to_response('plants/plant_detail.html', {'plant':plant, 'plant_url':getCanonicalPlantUrl(plant)},\
-		context_instance = RequestContext(request))
-	
+def detail(request, id):
+    try:
+        plant = Plant.objects.get(url_slug=id)
+    except ObjectDoesNotExist:
+        plant = get_object_or_404(Plant, site_code=id)
+
+    return render_to_response('plants/plant_detail.html',
+                              {'plant': plant, 'plant_url': getCanonicalPlantUrl(plant)},
+                              context_instance=RequestContext(request))
