028e33f202ffab0b4f6051b8d1d43016b5315336
Matthew Lapora <github@thelaporas.com>
2016-11-28 13:27:44 -0800

    Modify script to migrate over Locations

--- scripts/migrate-coords.py
+++ scripts/migrate-coords.py
@@ -1,14 +1,26 @@
-from plants.models import Plant
+# from plants.models import Plant
+from plants.models import Location
 from geoposition import Geoposition
 
 
+# def run():
+#     a = Plant.objects.all()
+
+#     for item in a:
+#         if item.coordinates:
+#             item.geo_location = Geoposition(latitude=item.coordinates.latitude,
+#                                             longitude=item.coordinates.longitude)
+#             item.save()
+#         else:
+#             pass
+
 def run():
-    a = Plant.objects.all()
+    a = Location.objects.all()
 
     for item in a:
-        if item.coordinates:
-            item.geo_location = Geoposition(latitude=item.coordinates.latitude, longitude=item.coordinates.longitude)
-
+        if item.gps_latitude and item.gps_longitude:
+            item.geo_location = Geoposition(latitude=item.gps_latitude,
+                                            longitude=item.gps_longitude)
             item.save()
         else:
             pass
