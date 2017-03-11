b0c711567a267ef1137f50914ad837f34d55b0ad
Matthew Lapora <github@thelaporas.com>
2017-03-11 15:11:39 -0800

    Remove stash/unstash as we fixed requirements.txt with conditional

--- scripts/TESTING-deploy-on-local-server.sh
+++ scripts/TESTING-deploy-on-local-server.sh
@@ -1,6 +1,4 @@
 cd $HOME/django-projects/test/beacon-food-forest-main/
-echo "============ Stashing local changes =============="
-git stash
 echo "============ Fetching new code from git =============="
 git fetch
 echo "Checking out branch $1 to test.beaconfoodforest.org"
@@ -13,8 +11,6 @@ export VIRTUALENVWRAPPER_PYTHON=$HOME/python3.5/bin/python3.5
 source $HOME/bin/virtualenvwrapper.sh
 workon bff-py3.5
 cd $HOME/django-projects/test/beacon-food-forest-main/
-echo "============ UN-Stashing local changes =============="
-git stash pop
 echo "============ Installing new requirements =============="
 pip install -r requirements.txt
 echo "============ Collecting new static files =============="
