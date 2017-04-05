033e1083135a93017c3ae9bea9e74d81e6b61384
Matthew Lapora <github@thelaporas.com>
2017-04-04 22:26:06 -0700

    Reset plants app migrations

--- plants/migrations/0001_initial.py
+++ plants/migrations/0001_initial.py
@@ -2,8 +2,9 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-import django_markdown.models
+import geoposition.fields
 import taggit.managers
+import django_markdown.models
 
 
 class Migration(migrations.Migration):
@@ -14,25 +15,12 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Coordinates',
-            fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
-                ('latitude', models.DecimalField(decimal_places=7, help_text='(Required) GPS latitude', default=0.0, max_digits=10)),
-                ('longitude', models.DecimalField(decimal_places=7, help_text='(Required) GPS longitude', default=0.0, max_digits=10)),
-            ],
-            options={
-                'verbose_name_plural': 'Coordinates',
-                'verbose_name': 'Coordinates',
-            },
-        ),
-        migrations.CreateModel(
             name='Location',
             fields=[
                 ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
-                ('gps_latitude', models.DecimalField(blank=True, decimal_places=7, help_text='(Optional) GPS latitude', default=0.0, max_digits=10)),
-                ('gps_longitude', models.DecimalField(blank=True, decimal_places=7, help_text='(Optional) GPS longitude', default=0.0, max_digits=10)),
-                ('friendly_location', models.CharField(blank=True, help_text='(Optional) Description of location', max_length=200)),
-                ('greater_area', models.CharField(blank=True, help_text='(Optional) Description of area like Phase2 Upper', max_length=200)),
+                ('friendly_location', models.CharField(help_text='(Optional) Description of location', blank=True, max_length=200)),
+                ('greater_area', models.CharField(help_text='(Optional) Description of area like Phase2 Upper', blank=True, max_length=200)),
+                ('geo_location', geoposition.fields.GeopositionField(help_text='Approximate gps coordinates of location', blank=True, max_length=42, null=True)),
             ],
             options={
                 'verbose_name_plural': 'Locations',
@@ -43,8 +31,8 @@ class Migration(migrations.Migration):
             name='MapCategory',
             fields=[
                 ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
-                ('category', models.CharField(blank=True, help_text='(Optional) On map, main         folder to categorize this plant, e.g. Trees, Shrubs, Herbals.  If left blank, will not         be displayed on map.  More folders can be added in admin interface.', max_length=100)),
-                ('icon', models.ImageField(blank=True, help_text='Primary icon for category.  Please use         svg format for optimum performance.', upload_to='', verbose_name='category icon')),
+                ('category', models.CharField(help_text='(Optional) On map, main         folder to categorize this plant, e.g. Trees, Shrubs, Herbals.  If left blank, will not         be displayed on map.  More folders can be added in admin interface.', blank=True, max_length=100)),
+                ('icon', models.ImageField(help_text='Primary icon for category.  Please use         svg format for optimum performance.', blank=True, verbose_name='category icon', upload_to='')),
             ],
             options={
                 'verbose_name_plural': 'Map Folder Category',
@@ -55,8 +43,8 @@ class Migration(migrations.Migration):
             name='MapSubCategory',
             fields=[
                 ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
-                ('subcategory', models.CharField(blank=True, help_text='(Optional) On map,         subfolder to categorize this plant, e.g. under trees, we would have -- Apples, Nuts,         etc.  If left blank, will not be displayed on map.  More subfolders can be added', max_length=100)),
-                ('icon', models.ImageField(blank=True, help_text='Primary icon for subcategory.  Please use         svg format for optimum performance.', upload_to='', verbose_name='subcategory icon')),
+                ('subcategory', models.CharField(help_text='(Optional) On map,         subfolder to categorize this plant, e.g. under trees, we would have -- Apples, Nuts,         etc.  If left blank, will not be displayed on map.  More subfolders can be added', blank=True, max_length=100)),
+                ('icon', models.ImageField(help_text='Primary icon for subcategory.  Please use         svg format for optimum performance.', blank=True, verbose_name='subcategory icon', upload_to='')),
             ],
             options={
                 'verbose_name_plural': 'Map Subfolder Category',
@@ -67,23 +55,23 @@ class Migration(migrations.Migration):
             name='Plant',
             fields=[
                 ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
-                ('friendly_name', models.CharField(blank=True, help_text="(Optional) A friendly plant name, like 'Charlie the Apple Tree'", max_length=100)),
-                ('latin_name', models.CharField(blank=True, help_text='(Optional) Latin name', max_length=150)),
+                ('friendly_name', models.CharField(help_text="(Optional) A friendly plant name, like 'Charlie the Apple Tree'", blank=True, max_length=100)),
+                ('latin_name', models.CharField(help_text='(Optional) Latin name', blank=True, max_length=150)),
                 ('name', models.CharField(help_text='Common Name (required)', max_length=150)),
-                ('text', django_markdown.models.MarkdownField(blank=True, help_text='(Optional) Descriptive text for plant (put anything here)')),
-                ('primary_picture', models.ImageField(blank=True, help_text='(Optional) primary image', upload_to='')),
-                ('year_planted', models.IntegerField(blank=True, help_text='(Optional) Year planted at Beacon Food forest.  Use -1 for unknown.', null=True)),
-                ('site_code', models.CharField(help_text='(Required) A unique code for plant, comprised of year planted +         sequential number of plant.  e.g. 1501 (2015,first plant)', max_length=20, unique=True)),
-                ('url_slug', models.SlugField(blank=True, help_text='(Optional) An url friendly short description.        Must be unique to each plant e.g julies-quince', unique=True)),
+                ('text', django_markdown.models.MarkdownField(help_text='(Optional) Descriptive text for plant (put anything here)', blank=True)),
+                ('primary_picture', models.ImageField(help_text='(Optional) primary image', blank=True, upload_to='')),
+                ('year_planted', models.IntegerField(help_text='(Optional) Year planted at Beacon Food forest.  Use -1 for unknown.', blank=True, null=True)),
+                ('site_code', models.CharField(unique=True, help_text='(Required) A unique code for plant, comprised of year planted +         sequential number of plant.  e.g. 1501 (2015,first plant)', max_length=20)),
+                ('url_slug', models.SlugField(unique=True, help_text='(Optional) An url friendly short description.        Must be unique to each plant e.g julies-quince', blank=True)),
                 ('last_modified', models.DateTimeField(auto_now=True)),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('alive', models.BooleanField(help_text='Is plant alive? Unchecked        declares dead but maintains info for historical purposes.', default=True)),
                 ('published', models.BooleanField(help_text='If you want to remove        the plant from being listed on the site, then uncheck this.', default=True)),
-                ('category', models.ForeignKey(blank=True, to='plants.MapCategory', null=True, help_text='(Optional) On map, main folder to categorize this plant, e.g. Trees, Shrubs,         Herbals.  If left blank, will not be displayed on map.  More folders can be added        in admin interface.')),
-                ('coordinates', models.ForeignKey(blank=True, to='plants.Coordinates', null=True, help_text='Exact gps coordinates of location of plant')),
-                ('location', models.ForeignKey(blank=True, to='plants.Location', null=True, help_text='Assign a location (made separately)')),
-                ('subcategory', models.ForeignKey(blank=True, to='plants.MapSubCategory', null=True, help_text='(Optional) On map, subfolder to categorize this plant, e.g. under trees,         we would have -- Apples, Nuts, etc.  If left blank, will not be displayed on map.          More subfolders can be added in admin interface.')),
-                ('tags', taggit.managers.TaggableManager(through='taggit.TaggedItem', to='taggit.Tag', help_text='A comma-separated list of tags.', verbose_name='Tags')),
+                ('geo_location', geoposition.fields.GeopositionField(help_text='Exact gps coordinates of location of plant', blank=True, max_length=42, null=True)),
+                ('category', models.ForeignKey(blank=True, help_text='(Optional) On map, main folder to categorize this plant, e.g. Trees, Shrubs,         Herbals.  If left blank, will not be displayed on map.  More folders can be added        in admin interface.', to='plants.MapCategory', null=True)),
+                ('location', models.ForeignKey(blank=True, help_text='Assign a location (made separately)', to='plants.Location', null=True)),
+                ('subcategory', models.ForeignKey(blank=True, help_text='(Optional) On map, subfolder to categorize this plant, e.g. under trees,         we would have -- Apples, Nuts, etc.  If left blank, will not be displayed on map.          More subfolders can be added in admin interface.', to='plants.MapSubCategory', null=True)),
+                ('tags', taggit.managers.TaggableManager(help_text='A comma-separated list of tags.', verbose_name='Tags', to='taggit.Tag', through='taggit.TaggedItem')),
             ],
             options={
                 'verbose_name_plural': 'plants',
