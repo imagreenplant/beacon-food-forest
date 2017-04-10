6f86c3fcd1276f74e9c9814b8c57c574d2c030bc
Matthew Lapora <github@thelaporas.com>
2017-04-09 20:52:55 -0700

    Add admin page for committee

--- committee/admin.py
+++ committee/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django_markdown.admin import MarkdownModelAdmin
+from .models import Committee, Meeting
 
-# Register your models here.
+@admin.register(Committee)
+class CommitteeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Meeting)
+class MeetingAdmin(MarkdownModelAdmin):
+    list_display = ('committee', 'date', 'author')
