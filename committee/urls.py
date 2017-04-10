d42fc5512119f7988675042e75c7883a1c579403
Matthew Lapora <github@thelaporas.com>
2017-04-09 20:54:52 -0700

    Activate committee hub page

--- committee/urls.py
+++ committee/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
-from . import views
+# from committee import views
+import committee
 
 urlpatterns = [
-    url(r'^$', 'views.index', name="committee-hub"),
+    url(r'^$', 'committee.views.index', name="committee-hub"),
     # url(r'^meetings/upcoming/$', 'views.current_classes',name="upcoming-meetings"),
     # url(r'^meetings/all/$', 'views.past_classes', name="past-meetings"),
-    url(r'^/meeting/(?P<slug>[\w-]+)/$',
-        'views.meeting_detail', name="meeting-detail"),
-    url(r'^(?P<committee>[\w-]+)/$',
-        'views.committee_detail', name="committee-detail"),
+    # url(r'^/meeting/(?P<slug>[\w-]+)/$',
+    #     'views.meeting_detail', name="meeting-detail"),
+    # url(r'^(?P<name>[\w-]+)/$',
+    #     'views.committee_detail', name="committee-detail"),
 ]
--- committee/views.py
+++ committee/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, get_object_or_404
+from .models import Committee, Meeting
+from django.template import RequestContext
 
-# Create your views here.
+def index(request):
+    committees = Committee.objects.all()
+    page_title = "Committees"
+
+    return render_to_response(
+        'committee/committee_hub.html', 
+        {'title': page_title, 'committees': committees},
+        context_instance=RequestContext(request))
+
+
+def past_meetings():
+    pass
+
+def upcoming_meetings():
+    pass
+
+def committee_detail():
+    pass
+
+def meeting():
+    pass
