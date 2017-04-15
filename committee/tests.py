7cf9c26312ff35b24b5775631e1653b23acfdaf9
Matthew Lapora <github@thelaporas.com>
2017-04-14 22:13:28 -0700

    Add committee hub test

--- committee/tests.py
+++ committee/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from committee.models import Committee, Meeting
 
 
 class ResponseStatus(TestCase):
@@ -11,5 +12,13 @@ class ResponseStatus(TestCase):
     def test_committee(self):
         response = self.client.get('/committee/')
         self.assertEqual(response.status_code, 200)
+
+    def test_committee_detail_resp(self):
+        committee = Committee(name="Committee Name", main_contact="Main Contact", slug="commitee-name")
+        committee.save()
+
+        response = self.client.get(committee.get_absolute_url())
+        self.assertEqual(response.status_code,200)
+
 
 
