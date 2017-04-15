e9992463456cb398fe34ee6204d88fbebd7fa8a5
Matthew Lapora <github@thelaporas.com>
2017-04-15 14:37:56 -0700

    Add response tests for plant detail page

--- plants/tests.py
+++ plants/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Plant
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+class ClassEventTestCase(TestCase):
+
+    def setUp(self):
+        self.plant = Plant.objects.create(name="Test Plant", site_code="1401")
+        self.plant.save()
+
+    def test_plant_detail_resp(self):
+        response = self.client.get(self.plant.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+
+
+class ResponseStatus(TestCase):
+
+    def test_plant_hub(self):
+        response = self.client.get(reverse('plants-hub'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_plant_json(self):
+        response = self.client.get(reverse('plants-index-json'))
+        self.assertEqual(response.status_code, 200)
