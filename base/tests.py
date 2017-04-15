730bcd79513434258df39171ba63726455d8d423
Matthew Lapora <github@thelaporas.com>
2017-04-15 14:31:05 -0700

    Organize main response test cases

--- base/tests.py
+++ base/tests.py
@@ -3,53 +3,13 @@ from django.test import TestCase
 
 class ResponseStatus(TestCase):
 
-    def test_homepage(self):
-        response = self.client.get('')
+    def test_thanks(self):
+        response = self.client.get('/thanks/')
         self.assertEqual(response.status_code, 200)
 
     def test_404(self):
         response = self.client.get('/asdlfaskdjfalsdkfj/')
         self.assertEqual(response.status_code, 404)
-
-    def test_faq(self):
-        response = self.client.get('/faq/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_project(self):
-        response = self.client.get('/project/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_permaculture(self):
-        response = self.client.get('/permaculture/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_how_started(self):
-        response = self.client.get('/how-we-started/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_get_involved(self):
-        response = self.client.get('/get-involved/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_thanks(self):
-        response = self.client.get('/thanks/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_faq_redir(self):
-        response = self.client.get('/faq.html')
-        self.assertEqual(response.status_code, 301)
-
-    def test_project_redir(self):
-        response = self.client.get('/project.html')
-        self.assertEqual(response.status_code, 301)
-
-    def test_permaculture_redir(self):
-        response = self.client.get('/permaculture.html')
-        self.assertEqual(response.status_code, 301)
-
-    def test_howstarted_redir(self):
-        response = self.client.get('/howwestarted.html')
-        self.assertEqual(response.status_code, 301)
 
     # def test_announcement(self):
     #     response = self.client.get('/faq/')
