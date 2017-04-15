ddfff37d5915bb536739aa62f245b2df4f6ba9e6
Matthew Lapora <github@thelaporas.com>
2017-04-14 22:36:13 -0700

    Slight refactor for setup, and added a 200 check

--- education/tests.py
+++ education/tests.py
@@ -5,13 +5,18 @@ from datetime import timedelta
 
 
 class ClassEventTestCase(TestCase):
+
     def setUp(self):
-        ClassEvent.objects.create(
-            title="Class within 4 days from now", 
-            event_date=localtime(now()) + timedelta(days=2)
-            )
+        self.classevent = ClassEvent.objects.create(
+            title="Class within 4 days from now",
+            event_date=localtime(now()).date() + timedelta(days=2)
+        )
 
     def test_days_away_if_event_is_close(self):
         """If a class is within 4 days, it will show as an announcement"""
-        two_days_away = ClassEvent.objects.get(title="Class within 4 days from now")
-        self.assertEqual(two_days_away.days_away(), 2)
+        # two_days_away = ClassEvent.objects.get(title="Class within 4 days from now")
+        self.assertEqual(self.classevent.days_away(), 2)
+
+    def test_class_detail_resp(self):
+        response = self.client.get(self.classevent.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
