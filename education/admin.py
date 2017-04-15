7fdade27f6a10d2d9c721c9ab5fa02acc225a4f9
Matthew Lapora <github@thelaporas.com>
2017-04-14 20:36:05 -0700

    Remove comment from education admin

--- education/admin.py
+++ education/admin.py
@@ -4,12 +4,10 @@ from markdownx.admin import MarkdownxModelAdmin
 
 from education.models import ClassEvent
 
-# @admin.site.register(ClassEvent)
-# class ClassEventAdmin(MarkdownModelAdmin):
-# 	list_display = ('title', 'class_slug_url', 'publish_date')
-
 admin.site.register(ClassEvent)
+
+
 class ClassEventAdmin(MarkdownxModelAdmin):
-	list_display = ('title', 'class_slug_url', 'publish_date')
-	prepopulated_fields = {"class_slug_url": ("title",)}
-	save_as = True
+    list_display = ('title', 'class_slug_url', 'publish_date')
+    prepopulated_fields = {"class_slug_url": ("title",)}
+    save_as = True
