7df5f82a9d39158ef1bd42ef91b9d8ba792dd2db
Matthew Lapora <github@thelaporas.com>
2017-04-15 14:22:11 -0700

    Remove unused urls and add response tests to active ones

--- maps/tests.py
+++ maps/tests.py
@@ -1,14 +1,18 @@
 from django.test import TestCase
 from maps.models import KmlMap
+from django.core.urlresolvers import reverse
 
 
 class ResponseStatus(TestCase):
 
     def setUp(self):
         self.testKML = KmlMap(name="Test KML", slug="test-kml", kml="blah blah blah")
-        print(self.testKML.get_absolute_url())
         self.testKML.save()
 
     def test_kml_map(self):
         response = self.client.get(self.testKML.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+
+    def test_fruit_trees(self):
+        response = self.client.get(reverse('fruit-trees'))
         self.assertEqual(response.status_code, 200)
--- maps/urls.py
+++ maps/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^fruit-trees/$', views.fruitTrees, name="fruit-trees"),
-    url(r'^categories/$', views.itemsByCategory, name="map-by-category"),
-    url(r'^(?P<tag>[\w-]+)/$', views.tag_map, name="tag-map"),
+    # url(r'^categories/$', views.itemsByCategory, name="map-by-category"),
+    # url(r'^(?P<tag>[\w-]+)/$', views.tag_map, name="tag-map"),
     url(r'^kml/(?P<slug>[\w-]+)/$', views.kml_map, name="kml-map"),
 ]
