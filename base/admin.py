fd3d7f2c5d7e908bdab0f800ebcddcae946659cf
Matthew Lapora <github@thelaporas.com>
2017-04-26 10:51:52 -0700

    Add announcements to revision history

--- base/admin.py
+++ base/admin.py
@@ -1,28 +1,34 @@
 from django.contrib import admin
-# from django_markdown.admin import MarkdownModelAdmin
+from reversion.admin import VersionAdmin
 from markdownx.admin import MarkdownxModelAdmin
 from .models import ExternalNewsArticle, WorkPartyEvent, Announcement, VolunteerContact, Download
 
+
 @admin.register(ExternalNewsArticle)
 class ExternalNewsArticleAdmin(admin.ModelAdmin):
-	list_display = ('external_publisher', 'news_slug', 'pub_date')
+    list_display = ('external_publisher', 'news_slug', 'pub_date')
+
 
 @admin.register(WorkPartyEvent)
 class WorkPartyEventAdmin(admin.ModelAdmin):
-	list_display = ('work_party_date',)
+    list_display = ('work_party_date',)
+
 
 @admin.register(Announcement)
-class AnnouncementAdmin(MarkdownxModelAdmin):
-	list_display = ('publish_date', 'slug',)
-	fields = ('headline','publish_date','expire_date','event_date','content','primary_image','link','override','slug')
+class AnnouncementAdmin(MarkdownxModelAdmin, VersionAdmin):
+    list_display = ('publish_date', 'slug',)
+    fields = ('headline', 'publish_date', 'expire_date', 'event_date',
+              'content', 'primary_image', 'link', 'override', 'slug')
+
 
 @admin.register(VolunteerContact)
 class VolunteerAdmin(admin.ModelAdmin):
-	list_display = ('volunteer_name', 'volunteer_email', 'volunteer_is_active',)
+    list_display = ('volunteer_name', 'volunteer_email', 'volunteer_is_active',)
+
 
 @admin.register(Download)
 class DownloadAdmin(admin.ModelAdmin):
-	list_display = ('display_name', 'download_file', 'publish_to_frontpage',)	
+    list_display = ('display_name', 'download_file', 'publish_to_frontpage',)
 
 
 # Integrate Markdown specifically for the flatpages data
