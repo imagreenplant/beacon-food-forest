a46f785527608ea9b8c5b1409d1e8457cf53f4e2
Matthew Lapora <github@thelaporas.com>
2017-04-25 22:27:03 -0700

    Add revision history to education

--- education/admin.py
+++ education/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
-# from django_markdown.admin import MarkdownModelAdmin
 from markdownx.admin import MarkdownxModelAdmin
-
+from reversion.admin import VersionAdmin
 from education.models import ClassEvent
 
 admin.site.register(ClassEvent)
 
 
-class ClassEventAdmin(MarkdownxModelAdmin):
+class ClassEventAdmin(MarkdownxModelAdmin, VersionAdmin):
     list_display = ('title', 'class_slug_url', 'publish_date')
     prepopulated_fields = {"class_slug_url": ("title",)}
     save_as = True
