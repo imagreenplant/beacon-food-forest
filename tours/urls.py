d94184f7c71b797c0ac1bbc6b9ad2116cff062f4
Matthew Lapora <github@thelaporas.com>
2017-06-11 15:43:19 -0700

    Add calendar to tours tree

--- tours/urls.py
+++ tours/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from tours.views import audio_tour
+from tours.views import audio_tour, calendar
 
 urlpatterns = [
     url(r'^audio/$', audio_tour, name="audio-tour"),
+    url(r'^calendar/$', calendar, name="google-tour-calendar"),
 ]
