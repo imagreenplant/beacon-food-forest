0d925047133643f52b49b6b85ba8eb094aac1321
Matthew Lapora <github@thelaporas.com>
2018-10-24 14:44:53 -0700

    Add response test for new pages

--- volunteers/tests.py
+++ volunteers/tests.py
@@ -7,3 +7,11 @@ class ResponseStatus(TestCase):
     def test_volunteers(self):
         response = self.client.get(reverse('volunteers-hub'))
         self.assertEqual(response.status_code, 200)
+
+    def test_log_indiv_hours(self):
+        response = self.client.get(reverse('log-individual-hours'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_log_group_hours(self):
+        response = self.client.get(reverse('log-group-hours'))
+        self.assertEqual(response.status_code, 200)
