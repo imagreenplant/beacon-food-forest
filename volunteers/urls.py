c1b58ed667797f19d225c28dcb884f7bc76ac812
Matthew Lapora <github@thelaporas.com>
2018-10-24 14:42:30 -0700

    Add volunteer logging buttons and split out volunteer hub page

--- volunteers/urls.py
+++ volunteers/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from volunteers.views import index
+from volunteers.views import index, individual, group
 
 urlpatterns = [
     url(r'^$', index, name="volunteers-hub"),
+    url(r'^log-individual-hours$', individual, name="log-individual-hours"),
+    url(r'^log-group-hours$', group, name="log-group-hours"),
 ]
